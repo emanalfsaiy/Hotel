5f2df3108c2d8d40d5cb4a3c7c01821504e1d4a1
shasheal <koka182001@gmail.com>
2022-03-23 12:22:20 +0300

    name

--- SQLhotel.sql
+++ SQLhotel.sql
@@ -16,9 +16,9 @@ VALUES ('star5');
 
 insert into[dbo].[Coustomer] (name_customer,phone_number,countre,proof_id )
 VALUES ('fatma','092453768 ', 'Egypt','3') 
-
 insert into[dbo].[Proo] (type_proof,proof_number )
 VALUES ( 'ÃÊ«“«·”›— ','1228446001')
+
 
 insert into[dbo].[Reservation] (id_customer,idemploy,Date_reservation,Date_departure,room_number,num_room )
 VALUES ('2','9','1-3-2022','1-4-2022', '100','1') 
